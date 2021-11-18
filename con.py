e2b17a7a3bb2bd01b8e16fe438d1ef23b02a76f4
myusername <myusername@myemaildomain.com>
2021-11-18 08:59:53 +0300

    add

--- con.py
+++ con.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
-from pprint import pprint
-#import re
+import schedule, time
 
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient('mongodb://devroot:devroot@localhost:27018/mydb?authSource=admin')
@@ -9,6 +8,34 @@ client = MongoClient('mongodb://devroot:devroot@localhost:27018/mydb?authSource=
 mydb = client["mydb"]
 #выбираем коллекцию
 mycol = mydb["to"]
+
+def job():
+    with open('/var/log/auth.log') as f:
+        contents = f.readlines()
+        new_contents_list=[]
+        for i in iter(contents):
+        # disconect,clesed
+            if 'root' in i:
+                new_contents_list.append(i)
+    # print(x) 
+    for i in range(len(new_contents_list)):
+# ищем все строки время которых больше последнего документа в db
+        new_contents_list = contents[i].split()
+        myquery = {"datatime": new_contents_list[0:3], "time_service": new_contents_list[2], "user": new_contents_list[3], "host": 'host', "level deng": "2", "event": ' '.join(new_contents_list[4:]), "object":"OC"}
+        mydoc = mycol.insert_one(myquery)
+
+schedule.every(5).to(10).seconds.do(job)
+# бесконечный цикл, проверяющий каждую секунду, не пора ли запустить задание 
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
+
+# remove dockument db
+mycol.remove({})
+
+
+
+
 
 # Функция выводящая hostname
 # x = subprocess.run("hostname", stdout=subprocess.PIPE)
@@ -19,10 +46,10 @@ mycol = mydb["to"]
 # x = "echo $HOSTNAME"
 # def sh():
 #     global x
-#     y = os.system("bash -c '%s'" % x)
+#     y = os.system("hostname")
 #     return y
 # z = sh()
-# print(z)
+
 
 # Поиск всех записей в коллекции
 # myquery = {  }
@@ -36,20 +63,3 @@ mycol = mydb["to"]
 # myquery = {"name": "John", "address": "Highway 37"}
 
 # mydoc = mycol.insert_one(myquery)
-
-
-with open('/var/log/auth.log') as f:
-    contents = f.readlines()
-    x=[]
-    for i in iter(contents):
-        if 'root' in i:
-            x.append(i)
-    # print(x) 
-for i in range(len(x)):
-# ищем все строки время которых больше последнего документа в db
-    x = contents[i].split()
-    myquery = {"datatime": x[0:3], "time_service": x[2], "user": x[3], "host": 'host', "level deng": "2", "event": ' '.join(x[4:]), "object":"OC"}
-    mydoc = mycol.insert_one(myquery)
-
-# remove dockument db
-#mycol.remove({})
--- host.py
+++ host.py
@@ -1,5 +1,5 @@
-import subprocess
-p = subprocess.Popen('hostname', stdout=subprocess.PIPE) # Success!
-output = p.stdout.read()
-# stdout = p.communicate()
-print(output)
+# import subprocess
+# p = subprocess.Popen('hostname', stdout=subprocess.PIPE) # Success!
+# output = p.stdout.read()
+# # stdout = p.communicate()
+# print(output)
