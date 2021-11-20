7d15a13c734cc41393b7b314440f6b455dfaa556
myusername <myusername@myemaildomain.com>
2021-11-20 13:11:44 +0300

    add db

--- con.py
+++ con.py
@@ -9,57 +9,69 @@ mydb = client["mydb"]
 #выбираем коллекцию
 mycol = mydb["to"]
 
-def job():
-    with open('/var/log/auth.log') as f:
-        contents = f.readlines()
-        new_contents_list=[]
-        for i in iter(contents):
-        # disconect,clesed
-            if 'root' in i:
-                new_contents_list.append(i)
+
+# def job():
+#     with open('/var/log/auth.log') as f:
+#         contents = f.readlines()
+#         new_contents_list=[]
+#         for i in iter(contents):
+#         # disconect,clesed
+#             if 'root' in i:
+#                 new_contents_list.append(i)
+#     # print(x) 
+#     for i in range(len(new_contents_list)):
+# # ищем все строки время которых больше последнего документа в db
+#         new_contents_list = contents[i].split()
+#         myquery = {"datatime": new_contents_list[0:3], "time_service": new_contents_list[2], "user": new_contents_list[3], "host": 'host', "level deng": "2", "event": ' '.join(new_contents_list[4:]), "object":"OC"}
+#         mydoc = mycol.insert_one(myquery)
+
+# schedule.every(5).to(10).seconds.do(job)
+# # бесконечный цикл, проверяющий каждую секунду, не пора ли запустить задание 
+# while 1:
+#     schedule.run_pending()
+#     time.sleep(1) 
+# #remove dockument db
+# mycol.remove({})
+
+
+
+#mycol.remove({})
+
+with open('/var/log/auth.log') as f:
+    contents = f.readlines()
+    new_contents_list=[]
+if mycol.count() == 0:
     # print(x) 
-    for i in range(len(new_contents_list)):
+        for i in range(len(contents)):
 # ищем все строки время которых больше последнего документа в db
+            new_contents_list = contents[i].split()
+            myquery = {"datatime": new_contents_list[0:3], "time_service": new_contents_list[2], "user": new_contents_list[3], "host": 'host', "level deng": "2", "event": ' '.join(new_contents_list[4:]), "object":"OC"}
+            mydoc = mycol.insert_one(myquery)
+else:
+    # находим последюю запись в db
+    x = mycol.find().sort('_id',1)
+    print(x)
+    k, s=[],[]
+    for i in x:
+        k = i.values()
+        s = list(k)
+        c = s[1]
+    # print(c)
+    search_last_db = 0
+    new_contents_list4=[]
+    for i in range(len(contents)):
+    # ищем все строки время которых больше последнего документа в db
         new_contents_list = contents[i].split()
-        myquery = {"datatime": new_contents_list[0:3], "time_service": new_contents_list[2], "user": new_contents_list[3], "host": 'host', "level deng": "2", "event": ' '.join(new_contents_list[4:]), "object":"OC"}
+        if c == new_contents_list[0:3]:
+             search_last_db = i + 1
+    print(search_last_db)
+    print(len(contents))
+    # print(contents[search_last_db:])
+
+    for i in range(len(contents[search_last_db:])):
+        new_contents_list4 = contents[search_last_db:]
+        new_contents_list4 = new_contents_list4[i].split()
+        # print(new_contents_list4)
+        myquery = {"datatime": new_contents_list4[0:3], "time_service": new_contents_list4[2], "user": new_contents_list4[3], "host": 'host', "level deng": "2", "event": ' '.join(new_contents_list4[4:]), "object":"OC"}
         mydoc = mycol.insert_one(myquery)
-
-schedule.every(5).to(10).seconds.do(job)
-# бесконечный цикл, проверяющий каждую секунду, не пора ли запустить задание 
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
-
-# remove dockument db
-mycol.remove({})
-
-
-
-
-
-# Функция выводящая hostname
-# x = subprocess.run("hostname", stdout=subprocess.PIPE)
-# output = x.stdout.read()
-# print(output)
-
-# subprocess.Popen("echo $HOSTNAME")
-# x = "echo $HOSTNAME"
-# def sh():
-#     global x
-#     y = os.system("hostname")
-#     return y
-# z = sh()
-
-
-# Поиск всех записей в коллекции
-# myquery = {  }
-
-#mydoc = mycol.find(myquery)
-
-#for x in mydoc:
-#  print(x) 
-
-# Добавить записи
-# myquery = {"name": "John", "address": "Highway 37"}
-
-# mydoc = mycol.insert_one(myquery)
+    
--- host.py
+++
@@ -1,5 +0,0 @@
-# import subprocess
-# p = subprocess.Popen('hostname', stdout=subprocess.PIPE) # Success!
-# output = p.stdout.read()
-# # stdout = p.communicate()
-# print(output)
